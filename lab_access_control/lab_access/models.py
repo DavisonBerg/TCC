af4f508bd4d0972b8f1484eef5e318727c3d7fde
Anderson Berg <andersonberg@gmail.com>
2017-10-20 00:37:12 -0300

    atualizando aluno

--- lab_access_control/lab_access/models.py
+++ lab_access_control/lab_access/models.py
@@ -6,16 +6,32 @@ Dica: Aqui eu crio os tipos de usuário e quais dados eles terão
 from django.db import models
 
 
+class Laboratorio(models.Model):
+    nome = models.TextField()
+
+    class Meta:
+        ordering = ('nome',)
+
+
+class Bancada(models.Model):
+    numero = models.IntegerField()
+    lab = models.ForeignKey('Laboratorio')
+
+    class Meta:
+        ordering = ('numero',)
+
+
 class Professor(models.Model):
     '''
     Classe Professor
-    '''    
+    '''
     nome = models.TextField()
     cpf = models.IntegerField()
     tag = models.TextField()
 
     class Meta:
         ordering = ('nome',)
+
 
 class Tecnico(models.Model):
     nome = models.TextField()
@@ -25,24 +41,15 @@ class Tecnico(models.Model):
     class Meta:
         ordering = ('nome',)
 
+
 class Aluno(models.Model):
     nome = models.TextField()
     cpf = models.IntegerField()
     tag = models.TextField()
+    lab = models.ForeignKey('Laboratorio', blank=True, null=True)
+    bancada = models.ForeignKey('Bancada', blank=True, null=True)
+    hora_inicio = models.DateTimeField(blank=True, null=True)
+    hora_fim = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         ordering = ('nome',)
-
-
-class Laboratorio(models.Model):
-    nome = models.TextField()
-
-    class Meta:
-        ordering = ('nome',)
-
-class Bancada(models.Model):
-    numero = models.IntegerField()
-    lab = models.ForeignKey('Laboratorio')
-
-    class Meta:
-        ordering = ('numero',)
