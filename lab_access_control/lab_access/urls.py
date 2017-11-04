e94b264fb1c91cfa58e59e1b0db9ba8d0690a95b
Davison <dbsd@bepid.cin.ufpe.br>
2017-11-04 20:53:36 -0300

    Alterando retrive para busca de tag real e atualizando alunos com tags

--- lab_access_control/lab_access/urls.py
+++ lab_access_control/lab_access/urls.py
@@ -5,19 +5,19 @@ from lab_access import views
 
 urlpatterns = [
     url(r'^professores/$',views.ProfessorList.as_view()),
-    url(r'^professores/(?P<tag>[0-9]+)/$', views.ProfessorRetrieve.as_view()),
+    url(r'^professores/(?P<tag>[a-z0-9]+)/$', views.ProfessorRetrieve.as_view()),
     url(r'^professores/create/$', views.ProfessorCreate.as_view()),
     url(r'^professores/update/(?P<pk>[0-9]+)/$', views.ProfessorUpdate.as_view()),
     url(r'^professores/delete/(?P<pk>[0-9]+)/$', views.ProfessorDelete.as_view()),
 
     url(r'^tecnicos/$',views.TecnicoList.as_view()),
-    url(r'^tecnicos/(?P<tag>[0-9]+)/$', views.TecnicoRetrieve.as_view()),
+    url(r'^tecnicos/(?P<tag>[a-z0-9]+)/$', views.TecnicoRetrieve.as_view()),
     url(r'^tecnicos/create/$', views.TecnicoCreate.as_view()),
     url(r'^tecnicos/update/(?P<pk>[0-9]+)/$', views.TecnicoUpdate.as_view()),
     url(r'^tecnicos/delete/(?P<pk>[0-9]+)/$', views.TecnicoDelete.as_view()),
 
     url(r'^alunos/$',views.AlunoList.as_view()),
-    url(r'^alunos/(?P<tag>[0-9]+)/$', views.AlunoRetrieve.as_view()),
+    url(r'^alunos/(?P<tag>[a-z0-9]+)/$', views.AlunoRetrieve.as_view()),
     url(r'^alunos/create/$', views.AlunoCreate.as_view()),
     url(r'^alunos/update/(?P<pk>[0-9]+)/$', views.AlunoUpdate.as_view()),
     url(r'^alunos/delete/(?P<pk>[0-9]+)/$', views.AlunoDelete.as_view()),
